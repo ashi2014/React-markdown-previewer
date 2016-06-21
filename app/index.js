4949fc320b149ed89f1a5b91145811923a1032b4
Shi Angela <Angela.Shi@nagra.com>
2016-06-20 18:18:43 -0700

    Added basic functionality to app

--- app/index.js
+++ app/index.js
@@ -1,13 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var marked = require('marked');
 
-var HelloWorld = React.createClass({
+var MarkdownPreviewer = React.createClass({
+  getInitialState: function() {
+    return {
+        textString: ""
+    }
+  },
+  handleInputChange: function(e) {
+    this.setState({
+        textString: e.target.value
+    });
+  },
+  getMarkdown: function() {
+    return {__html: marked(this.state.textString)}
+  },
+
   render: function(){
     return (
-      <div>
-        Test
-      </div>
+        <div className="container">
+            <h1 className="text-center">Markdown Previewer</h1>
+            <div className="col-sm-8 col-sm-offset-2" style={{background: "pink"}}>
+                <div className="col-sm-6">
+                    <textarea rows="20" cols="50" onChange={this.handleInputChange}></textarea>
+                </div>
+                <div className="col-sm-6 preview-area" style={{background: "blue"}} dangerouslySetInnerHTML={this.getMarkdown()}>
+                </div>
+            </div>
+        </div>
     )
   }
 });
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById('app'));
